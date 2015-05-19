724a2f66bbcc0e8d2ef48ad38deb1a2343f2b5e5
James Li <yueli.m@gmail.com>
2015-05-19 20:40:00 +0000

    Run git clone in a docker container
    Running git clone command in a docker container will help
    constrain the resources of memory, CPU, disk and network bandwidth
    to prevent Solum from the DoS attacks of a malicious git server.
    
    Change-Id: I2f6a22e18bdcb4543d4177dde294ec6ae5072eba

--- solum/worker/lp_handlers/base.py
+++ solum/worker/lp_handlers/base.py
@@ -18,12 +18,16 @@ from docker import client as docker_client
 from oslo.config import cfg
 
 from solum.uploaders import tenant_logger
+from solum.worker.lp_handlers import utils
 
 cfg.CONF.import_opt('task_log_dir', 'solum.worker.config', group='worker')
 cfg.CONF.import_opt('docker_daemon_url', 'solum.worker.config', group='worker')
 log_dir = cfg.CONF.worker.task_log_dir
 docker_daemon_url = cfg.CONF.docker_daemon_url
+docker_cmd_runner = cfg.CONF.run_container_cmd_as
 
+MAX_GIT_CLONE_RETRY = 5
+GIT_CLONE_TIMEOUT = 900 # 15 minutes
 
 class BaseHandler(object):
 
@@ -32,10 +36,34 @@ class BaseHandler(object):
         self.assembly = assembly
         self._docker = None
 
-    def _clone_repo(self, repo_url, destination, commit_sha=None, branch=None):
+    def _clone_repo(self, repo_url, destination, revision='master'):
         # Clone a repo with the constraints of disk and memory usage
         # Need to consider limiting network bandwidth as well.
-        pass
+        container_dest = '/tmp/code'
+        if utils.is_git_sha(revision):
+            clone_cmd = ('git clone {url} {dst} &&'
+                         ' cd {dst}'
+                         ' git checkout -B solum {rev}').format(
+                url=repo_url, dst=container_dest, rev=revision)
+        else:
+            clone_cmd = ('git clone -b {branch} --depth 1'
+                         ' {url} {dst}').format(
+                branch=revision, url=repo_url, dst=container_dest)
+
+        timeout_clone = ('timeout --signal=SIGKILL {t}'
+                         ' /bin/bash -c \"{clone}\"').format(
+            t=GIT_CLONE_TIMEOUT, clone=clone_cmd)
+        cmd = ('RETRIES=0;'
+               ' until [ $RETRIES -ge 5 ]; do'
+               ' rm -rf {dst};'
+               ' {clone_cmd} && break;'
+               ' RETRIES=$[$RETRIES+1];'
+               ' sleep 5;'
+               ' done').format(dst=container_dest, clone_cmd=timeout_clone)
+        dockerfile = ('FROM solum/cloner\n'
+                      'RUN useradd -s /bin/bash -u {uid} solum-cloner\n'
+                      'USER solum-cloner\n'
+                      'CMD {cmd}').format(uid=docker_cmd_runner, cmd=cmd)
 
     def _validate_pub_repo(self, repo_url):
         pass
